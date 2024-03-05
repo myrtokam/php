558e2100df73999ebf21c4c40a169ba3f485cbb7
myrsini kampa <115368058+myrtokam@users.noreply.github.com>
2024-03-05 15:37:34 +0200

    Add files via upload

---
+++ 3-lucky numbers game.php
@@ -0,0 +1,61 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<p>give a number for the game:</p>
+<form method="POST" action="martios1.php">
+    first number  <input type="number" min='1' max="45" name="numbers[]"><br>
+    second number <input type="number" min='1' max="45" name="numbers[]"><br>
+    third number  <input type="number" min='1' max="45" name="numbers[]"><br>
+    fourth number <input type="number" min='1' max="45" name="numbers[]"><br>
+    fifth number  <input type="number" min='1' max="45" name="numbers[]"><br>
+    <p>give a joker number</p>
+    <input type="number" min='1' max="20" name="joker"><br>
+    <input type="submit" value="play" name="play"><br>
+</form>
+
+<?php
+if(isset($_POST['play']))
+{
+    $numbers = $_POST["numbers"];
+    $joker = $_POST["joker"];
+     sort($numbers);
+
+     if(count($numbers)!=count(array_unique($numbers)))
+     {
+        echo"<h3> try again! you gave duplicate numbers!</h3>";
+     }
+     else 
+     {
+
+    echo "<table border='1' width='300' height='300'>";
+    echo "<tr><th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>joker</th></tr>";
+    echo "<tr>";
+    for($i=0; $i<count($numbers); $i++)  // Fixed the typo here, changed $1 to $i
+    {
+        echo "<td>$numbers[$i]</td>";
+    }
+    echo"<td>$joker</td>";
+   
+    echo "</tr>";
+    
+    echo "</table>";
+}
+do{
+$lottery=array(rand(1,45),rand(1,45)rand(1,45),rand(1,45),rand(1,45));
+$lo_joker=rand(1,20);
+sort($lottery);
+}
+while(count($lottery)!=count(array_unique($lottery)));
+echo"<h3>the lucky numbers are:</h3>";
+
+}
+?>
+
+</body>
+</html>
